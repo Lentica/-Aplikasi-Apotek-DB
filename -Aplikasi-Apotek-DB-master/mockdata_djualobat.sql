763f172933cac0f497708e836f939379a333836f
heartbreaktwo <kakaafriz@gmail.com>
2019-05-15 23:55:38 -0700

    mock data for obat
    hjual and djual

---
+++ -Aplikasi-Apotek-DB-master/mockdata_djualobat.sql
@@ -0,0 +1,5 @@
+insert into djualobat values('NO001','ME001',3,58395);
+insert into djualobat values('NO002','NA001',2,127461);
+insert into djualobat values('NO003','CA001',4,87441);
+insert into djualobat values('NO004','AM001',3,32128);
+insert into djualobat values('NO005','CG001',1,130010);
---
+++ -Aplikasi-Apotek-DB-master/mockdata_hjualobat.sql
@@ -0,0 +1,5 @@
+insert into hjualobat values('','DC001',to_date('02/01/2019','dd/mm/yyyy'),3);
+insert into hjualobat values('','GS001',to_date('12/01/2019','dd/mm/yyyy'),2);
+insert into hjualobat values('','WH001',to_date('28/02/2019','dd/mm/yyyy'),4);
+insert into hjualobat values('','PT001',to_date('13/03/2019','dd/mm/yyyy'),3);
+insert into hjualobat values('','GM001',to_date('29/04/2019','dd/mm/yyyy'),1);
